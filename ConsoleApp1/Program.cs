4ab8880e2e6c7592241e0ed96097cc002a02e138
Muhammet Ali Çamlıca <Mecatr60@gmail.com>
2022-12-30 10:46:42 +0300

    Proje dosyası ekle.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace kdv.hesaplama;
+
+class Program
+
+
+{
+    static void Main(string[] args)
+
+    {
+        Console.WriteLine("1.NOTU GİRİNİZ");
+        int not1 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("2.NOTU GİRİNİZ");
+        int not2 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("SÖZLÜ NOTU GİRİNİZ");
+        int sozlu = int.Parse(Console.ReadLine());
+
+        var islem = not1 + not2 + sozlu;
+
+        var sonuc = islem / 3;
+
+        if (sonuc >= 50)
+        {
+            Console.WriteLine("ÖĞRENCİ" + "   " + sonuc + "   " + "ORTALAMASI İLE GEÇTİ");
+        }
+        else
+        {
+            Console.WriteLine("ÖĞRENCİ" + "   " + sonuc + "   " + "ORTALAMASI İLE KALDI");
+        }
+
+
+        Console.ReadKey();
+    }
+
+}
